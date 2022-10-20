ca9c0e8591a0a794e2d08c79a3a0e4c214f3f7ed
SamanthaBooth81 <samanthabooth09@live.co.uk>
2022-10-20 12:05:51 +0000

    start new django project, app and homepage view

---
+++ home/urls.py
@@ -0,0 +1,7 @@
+"""Home urls"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+]
---
+++ home/views.py
@@ -0,0 +1,7 @@
+"""Home Views"""
+from django.shortcuts import render
+
+
+def index(request):
+    """View to return index page"""
+    return render(request, 'home/index.html')
